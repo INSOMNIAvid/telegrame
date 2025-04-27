993050709cc56d45958aa5910ded2a688e71e2d1
INSOMNIAvid <scone_godlier_9l@icloud.com>
2025-04-27 16:09:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAryOAJtH9AxUBBzPTdNMyhapUvSzxAREs",
   authDomain: "edfghj-eea58.firebaseapp.com",
@@ -6,6 +6,7 @@ const firebaseConfig = {
   storageBucket: "edfghj-eea58.firebasestorage.app",
   messagingSenderId: "441274161947",
   appId: "1:441274161947:web:79a344c0121d0d0bfe91be",
+  measurementId: "G-7KZCMLECJM"
 };
 
 // Initialize Firebase
